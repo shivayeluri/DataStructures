cbe55753baaf1e9eb8aa8a367b4dc72675179f41
shivakumar.y <shivakumar.y@perfios.com>
2022-06-07 18:49:16 +0530

    Added : To implement Queue class

---
+++ src/dataStructures/Que.java
@@ -0,0 +1,112 @@
+package dataStructures;
+
+import java.util.Scanner;
+
+public class Que {
+    int SIZE;
+    int[] items;
+    int front;
+    int rear;
+
+    public Que(int size) {
+        SIZE = size;
+        items = new int[SIZE];
+        front = -1;
+        rear = -1;
+    }
+
+    public void operation() {
+        System.out.println(" ");
+        System.out.println("Choose one operation");
+        System.out.print("1. enqueue\n2. dequeue\n3. print\n4. quit");
+        System.out.println(" ");
+        boolean counter = false;
+
+        while(!counter) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("select operation");
+            int option = sc.nextInt();
+            if (option > 0 && option < 5) {
+                switch (option) {
+                    case 1:
+                        Scanner sc1 = new Scanner(System.in);
+                        System.out.println("what you want to enqueue");
+                        int input = sc1.nextInt();
+                        enQueue(input);
+                        display();
+                        break;
+                    case 2:
+                        deQueue();
+                        break;
+                    case 3:
+                        display();
+                        break;
+                    case 4:
+                        display();
+                        counter = true;
+                        System.exit(-1);
+                }
+            }
+        }
+
+    }
+
+    boolean isQueFull() {
+        return front == 0 && rear == SIZE - 1;
+    }
+
+    boolean isQueEmpty() {
+        return front == -1;
+    }
+
+    void enQueue(int element) {
+        if (isQueFull()) {
+            System.out.println("Queue is full");
+            System.exit(-1);
+        } else {
+            if (front == -1) {
+                front = 0;
+            }
+
+            ++rear;
+            items[rear] = element;
+            System.out.println("Insert " + element);
+        }
+
+    }
+
+    void deQueue() {
+        if (isQueEmpty()) {
+            System.out.println("Queue is empty");
+        } else {
+            int element = items[front];
+            if (front >= rear) {
+                front = -1;
+                rear = -1;
+            } else {
+                ++front;
+            }
+
+            System.out.println("" + element + " Deleted");
+        }
+
+    }
+
+    void display() {
+        if (isQueEmpty()) {
+            System.out.println("Empty Queue");
+        } else {
+            System.out.println("\nFront index-> " + front);
+            System.out.println("Items -> ");
+
+            for(int i = front; i <= rear; ++i) {
+                int value = items[i];
+                System.out.print("" + value + "  ");
+            }
+
+            System.out.println("\nRear index-> " + this.rear);
+        }
+
+    }
+}
+
