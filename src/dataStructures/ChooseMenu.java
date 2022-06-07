411f00082036deef5bf2c0ea1e9f4b1ee8f6e391
shivakumar.y <shivakumar.y@perfios.com>
2022-06-07 20:13:15 +0530

    Modified : modified Choose menu class

--- src/dataStructures/ChooseMenu.java
+++ src/dataStructures/ChooseMenu.java
@@ -17,31 +17,37 @@ public class ChooseMenu {
         sb.append("1. Linked List\n2. Ordered Linked List\n3. Queue\n4. Stack");
         System.out.println(sb);
         Scanner input = new Scanner(System.in);
+        int counter = 0;
 
         try {
-            if (option > 0 && option < 5) {
+            if (option > 0 && option < 6) {
                 int size;
                 switch (option) {
-                    case 1:
+                    case 1 -> {
                         System.out.println("You have chose Linked list ");
                         new LinkedList().operations();
-                    case 2:
-                        System.out.println("Ordered Linked List");
-                        break;
-                    case 3:
+                    }
+                    case 2 -> System.out.println("Ordered Linked List");
+                    case 3 -> {
                         System.out.println("Please enter Queue size :");
                         int queueSize = input.nextInt();
                         new Que(queueSize).operation();
-                    case 4:
+                    }
+                    case 4 -> {
                         System.out.println("Please enter stack size :");
                         int stackSize = input.nextInt();
                         new Stack(stackSize).operations();
+                    }
+                    case 5 -> {
+                        System.out.println("Good bye");
+                        counter = 5;
+                    }
                 }
             } else {
-                System.out.println("\nPlease choose 1-4 only");
+                System.out.println("\nPlease choose 1-5 only");
             }
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println("Please enter integers only !");
         }
 
     }
